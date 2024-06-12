5fc5777fdc3a7315cf0224aacef4f32c2ab42025
CharlieHii <charlie_chaper@hotmail.com>
2024-06-12 11:17:56 +0700

    feat, style: complete required requirement

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import axios from "axios";
 function App() {
   const [searchText, setSearchText] = useState("");
   const [touristData, setTouristData] = useState([]);
+  const [isContinueReading, setIsContinueReading] = useState(false);
   const getTouristData = async () => {
     const result = await axios.get(
       `http://localhost:4001/trips?keywords=${searchText}`
@@ -16,43 +17,105 @@ function App() {
     getTouristData();
   }, [searchText]);
   return (
-    <div className="App">
+    <div className="App flex flex-col items-center">
       {/* Start coding here */}
-      <h1>เที่ยวไหนดี</h1>
-      <div>
-        <h2>ค้นหาที่เที่ยว</h2>
-        <input
-          placeholder="หาที่เที่ยวแล้วไปกัน ..."
-          onChange={(event) => setSearchText(event.target.value)}
-        />
-      </div>
-      {/* description eid photos tags title url */}
-      {touristData.map((item) => (
-        <div key={item.eid}>
-          <img src={item.photos[0]} />
-          <article>
-            <h3>{item.title}</h3>
-            <p>{item.description}</p>
-            <div>
-              <span>หมวด</span>
-              {item.tags.map((tag, tagIndex) =>
-                tagIndex === tag.length - 1 ? (
-                  <span key={tagIndex}>
-                    และ <span>{tag}</span>
-                  </span>
-                ) : (
-                  <span key={tagIndex}>{tag}</span>
-                )
-              )}
-            </div>
-            <div>
-              {item.photos.slice(1).map((photo, photoIndex) => (
-                <img key={photoIndex} src={photo} />
-              ))}
-            </div>
-          </article>
+      <header className="w-full flex flex-col items-center">
+        <h1 className="text-5xl text-dodgerblue text-center font-black mt-14">
+          เที่ยวไหนดี
+        </h1>
+        <div className=" w-[80%] mt-7">
+          <h2 className=" text-sm">ค้นหาที่เที่ยว</h2>
+          <input
+            placeholder="หาที่เที่ยวแล้วไปกัน ..."
+            onChange={(event) => setSearchText(event.target.value)}
+            className="text-center text-sm w-full border-b-2"
+          />
         </div>
-      ))}
+      </header>
+      {/* description eid photos tags title url */}
+      <main className="px-10 w-full">
+        {touristData.map((item) => (
+          <div key={item.eid} className="flex mt-10 gap-8">
+            <img
+              src={item.photos[0]}
+              className="w-[305px] h-[215px] rounded-3xl object-cover"
+            />
+            <article className="text-gray-500 relative">
+              <h3 className=" text-xl font-bold text-black">{item.title}</h3>
+              <div className=" text-sm ">
+                {item.description.length <= 100 ? (
+                  <p>{item.description}</p>
+                ) : isContinueReading ? (
+                  <p>
+                    <span>{item.description} </span>
+                    <button
+                      onClick={() => setIsContinueReading(!isContinueReading)}
+                      className=" text-blue-400"
+                    >
+                      {" "}
+                      ยาวไปไม่อ่าน
+                    </button>
+                  </p>
+                ) : (
+                  <p>
+                    <span>{item.description.slice(0, 100)}</span>
+                    <button
+                      onClick={() => setIsContinueReading(!isContinueReading)}
+                      className=" text-blue-400"
+                    >
+                      ... อ่านต่อ
+                    </button>
+                  </p>
+                )}
+              </div>
+              <div>
+                <span>หมวด</span>
+                {item.tags.map((tag, tagIndex) =>
+                  tagIndex === item.tags.length - 1 ? (
+                    <span key={tagIndex}>
+                      {" "}
+                      และ{" "}
+                      <a
+                        href={`https://www.google.com/search?q=${tag}`}
+                        target="_blank"
+                        className=" underline"
+                      >
+                        {tag}
+                      </a>
+                    </span>
+                  ) : (
+                    <span key={tagIndex}>
+                      {" "}
+                      <a
+                        href={`https://www.google.com/search?q=${tag}`}
+                        target="_blank"
+                        className=" underline"
+                      >
+                        {tag}
+                      </a>
+                    </span>
+                  )
+                )}
+              </div>
+              <div className="flex gap-8 mt-3">
+                {item.photos.slice(1).map((photo, photoIndex) => (
+                  <img
+                    key={photoIndex}
+                    src={photo}
+                    className="w-[100px] h-[100px] object-cover rounded-xl"
+                  />
+                ))}
+              </div>
+              <button
+                className="absolute right-2 bottom-2 rounded-full"
+                onClick={() => open(item.url, "_blank")}
+              >
+                Click Me
+              </button>
+            </article>
+          </div>
+        ))}
+      </main>
     </div>
   );
 }
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        dodgerblue: "rgb(47,156,220)",
+        lightblue: "rgb(159,205,237)",
+      },
+    },
   },
   plugins: [],
 };
